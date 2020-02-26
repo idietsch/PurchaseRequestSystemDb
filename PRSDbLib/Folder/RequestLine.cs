b6dab4b9b109841c6508eb89696d12eaa8fa78a4
Ian Dietsch <wolffox36007@gmail.com>
2020-02-26 09:35:30 -0500

    Add project files.

---
+++ PRSDbLib/Folder/RequestLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRSDbLib.Folder {
+    public class RequestLine {
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ PurchaseRequestSystemDb/Program.cs
@@ -0,0 +1,39 @@
+﻿using PRSDbLib;
+using PRSDbLib.Folder;
+using System;
+using System.Linq;
+
+namespace PurchaseRequestSystemDb {
+    class Program {
+        static void Main(string[] args) {
+            
+
+
+
+
+            static void AddUser(AppDbContext context) {
+                var use = new User {
+                    Id = 0,
+                    Username = "Username1",
+                    Password = "Password1",
+                    Firstname = "John",
+                    Lastname = "Johnson",
+                    Phone = null,
+                    Email = "email1@email1.com",
+                    IsReviewer = true,
+                    IsAdmin = false
+                };
+                context.Users.Add(use);
+                var rowsAffected = context.SaveChanges();
+                if (rowsAffected != 1) throw new Exception("User Add Failed");
+                else { Console.WriteLine("User Add Successful"); }
+            }
+            static void CheckForUser(AppDbContext context) {
+                var usrs = context.Users.ToList();
+                if (usrs.Exists()) {
+
+                }
+            }
+        }
+    }
+}
